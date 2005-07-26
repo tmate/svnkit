c76987e0863aa00f779948cb0fd724660a0bf785
eugeny <eugeny@0a862816-5deb-0310-9199-c792c6ae6c6e>
2005-07-26 11:55:20 +0000

    Some bugs fixed.

--- javasvn/src/org/tmatesoft/svn/core/internal/io/dav/BasicHttpMethod.java
+++ javasvn/src/org/tmatesoft/svn/core/internal/io/dav/BasicHttpMethod.java
@@ -3,6 +3,7 @@
  */
 package org.tmatesoft.svn.core.internal.io.dav;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -11,16 +12,30 @@ import org.apache.commons.httpclient.HttpConnection;
 import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.HttpMethodBase;
 import org.apache.commons.httpclient.HttpState;
+import org.tmatesoft.svn.core.SVNException;
 
 public class BasicHttpMethod extends HttpMethodBase{
     
     private final String myName;
-    private final InputStream myRequestBody;
+    private final byte[] myRequestBodyArray;
     
-    public BasicHttpMethod(String name, String uri, InputStream requestBody){
+    public BasicHttpMethod(String name, String uri, InputStream requestBody) throws SVNException{
         super(uri);
         myName = name;
-        myRequestBody = requestBody;
+        try{
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            while (true) {
+                int count = requestBody.read(buffer);
+                if (count <= 0) {
+                    break;
+                }
+                os.write(buffer, 0, count);
+            }
+            myRequestBodyArray = os.toByteArray();
+        }catch(IOException e){
+            throw new SVNException(e);
+        }
     }
     
     public String getName() {
@@ -29,14 +44,7 @@ public class BasicHttpMethod extends HttpMethodBase{
 
     protected boolean writeRequestBody(HttpState state, HttpConnection conn) throws IOException, HttpException {
         OutputStream os = conn.getRequestOutputStream();
-        byte[] buffer = new byte[1024];
-        while (true) {
-            int count = myRequestBody.read(buffer);
-            if (count <= 0) {
-                break;
-            }
-            os.write(buffer, 0, count);
-        }
+        os.write(myRequestBodyArray);
         return true;
     }
 }
