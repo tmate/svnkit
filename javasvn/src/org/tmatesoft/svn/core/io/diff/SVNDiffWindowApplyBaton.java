f013f2522ea5df0f642c44b331109cf0c1ed35ef
sa <sa@0a862816-5deb-0310-9199-c792c6ae6c6e>
2005-10-12 05:23:01 +0000

    implemented getFile() for FSRepository

---
+++ javasvn/src/org/tmatesoft/svn/core/io/diff/SVNDiffWindowApplyBaton.java
@@ -0,0 +1,74 @@
+/*
+ * ====================================================================
+ * Copyright (c) 2004 TMate Software Ltd.  All rights reserved.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at http://tmate.org/svn/license.html.
+ * If newer versions of this license are posted there, you may use a
+ * newer version instead, at your option.
+ * ====================================================================
+ */
+package org.tmatesoft.svn.core.io.diff;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.MessageDigest;
+
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.internal.wc.SVNFileUtil;
+
+
+/**
+ * @version 1.0
+ * @author  TMate Software Ltd.
+ */
+public class SVNDiffWindowApplyBaton {
+    
+    InputStream mySourceStream;
+    OutputStream myTargetStream;
+    
+    long mySourceViewOffset;
+    long mySourceViewLength;
+    long myTargetViewSize;
+    
+    byte[] mySourceBuffer;
+    byte[] myTargetBuffer;
+    MessageDigest myDigest;
+    
+    public static SVNDiffWindowApplyBaton create(File source, File target, MessageDigest digest) throws SVNException {
+        SVNDiffWindowApplyBaton baton = new SVNDiffWindowApplyBaton();
+        baton.mySourceStream = source.exists() ? SVNFileUtil.openFileForReading(source) : SVNFileUtil.DUMMY_IN;
+        baton.myTargetStream = SVNFileUtil.openFileForWriting(target, true);
+        baton.mySourceBuffer = new byte[0];
+        baton.mySourceViewLength = 0;
+        baton.mySourceViewOffset = 0;
+        baton.myDigest = digest;
+        return baton;
+    }
+    
+    public static SVNDiffWindowApplyBaton create(InputStream source, OutputStream target, MessageDigest digest) {
+        SVNDiffWindowApplyBaton baton = new SVNDiffWindowApplyBaton();
+        baton.mySourceStream = source != null ? source : SVNFileUtil.DUMMY_IN;
+        baton.myTargetStream = target;
+        baton.mySourceBuffer = new byte[0];
+        baton.mySourceViewLength = 0;
+        baton.mySourceViewOffset = 0;
+        baton.myDigest = digest;
+        return baton;
+    }
+
+    private SVNDiffWindowApplyBaton() {
+    }
+
+    public String close() {
+        SVNFileUtil.closeFile(mySourceStream);
+        SVNFileUtil.closeFile(myTargetStream);
+        if (myDigest != null) {
+            return SVNFileUtil.toHexDigest(myDigest);
+        }
+        return null;
+    }
+
+}
