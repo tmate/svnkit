c62965eaa0b7b99c85e517e2fef18f45c70d0e50
alex <alex@0a862816-5deb-0310-9199-c792c6ae6c6e>
2007-03-21 18:32:54 +0000

    branch for DAV servlet

---
+++ branches/servlet/svnkit/src/org/tmatesoft/svn/core/internal/io/dav/servlet/DAVServlet.java
@@ -0,0 +1,31 @@
+/*
+ * ====================================================================
+ * Copyright (c) 2004-2007 TMate Software Ltd.  All rights reserved.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at http://svnkit.com/license.html.
+ * If newer versions of this license are posted there, you may use a
+ * newer version instead, at your option.
+ * ====================================================================
+ */
+package org.tmatesoft.svn.core.internal.io.dav.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * @version 1.0
+ * @author  TMate Software Ltd.
+ */
+public class DAVServlet extends HttpServlet {
+    
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+
+}
