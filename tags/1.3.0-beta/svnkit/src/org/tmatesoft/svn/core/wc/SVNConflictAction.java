dc49fa4e91b85e1cb964f448d30b2dcb5f02d01f
sa <sa@0a862816-5deb-0310-9199-c792c6ae6c6e>
2009-04-14 12:20:20 +0000

    SVNKit version 1.3.0-beta tagged

---
+++ tags/1.3.0-beta/svnkit/src/org/tmatesoft/svn/core/wc/SVNConflictAction.java
@@ -0,0 +1,63 @@
+/*
+ * ====================================================================
+ * Copyright (c) 2004-2008 TMate Software Ltd.  All rights reserved.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at http://svnkit.com/license.html.
+ * If newer versions of this license are posted there, you may use a
+ * newer version instead, at your option.
+ * ====================================================================
+ */
+package org.tmatesoft.svn.core.wc;
+
+
+/**
+ * The <b>SVNConflictAction</b> represents the type of action being attempted on an object which leads to 
+ * a conflict.  
+ * 
+ * @version 1.2.0
+ * @author  TMate Software Ltd.
+ * @since   1.2.0
+ */
+public class SVNConflictAction {
+    /**
+     * Constant representing an attempt to change text or props.
+     */
+    public static final SVNConflictAction EDIT = new SVNConflictAction("edited");
+    /**
+     * Constant representing an attempt to add an object.
+     */
+    public static final SVNConflictAction ADD = new SVNConflictAction("added");
+    /**
+     * Constant representing an attempt to delete an object.
+     */
+    public static final SVNConflictAction DELETE = new SVNConflictAction("deleted");
+
+    public static SVNConflictAction fromString(String action) {
+        if (EDIT.getName().equals(action)) {
+            return EDIT;
+        }
+        if (ADD.getName().equals(action)) {
+            return ADD;
+        }
+        if (DELETE.getName().equals(action)) {
+            return DELETE;
+        }
+        return null;
+    }
+
+    private final String myName;
+
+    private SVNConflictAction(String name) {
+        myName = name;
+    }
+
+    public String getName() {
+        return myName;
+    }
+
+    public String toString() {
+        return getName();
+    }
+}
