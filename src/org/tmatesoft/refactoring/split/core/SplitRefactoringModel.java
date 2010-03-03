844563b659fe6db2c8cbf1c9b0508e195fd9d6c6
sergey.scherbina <sergey.scherbina@0a862816-5deb-0310-9199-c792c6ae6c6e>
2010-03-03 06:28:56 +0000

    Split script implementation in progress.

---
+++ src/org/tmatesoft/refactoring/split/core/SplitRefactoringModel.java
@@ -0,0 +1,164 @@
+package org.tmatesoft.refactoring.split.core;
+
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.ToolFactory;
+import org.eclipse.jdt.core.formatter.CodeFormatter;
+import org.eclipse.jdt.core.search.IJavaSearchScope;
+import org.eclipse.jdt.core.search.SearchEngine;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ltk.core.refactoring.Change;
+
+public class SplitRefactoringModel {
+
+	private String sourcePackageName;
+	private String targetPackageName;
+	private String targetSuffix;
+	private List<String> typesToHideNames;
+	private IStructuredSelection selection;
+	private IProject project;
+	private IJavaProject javaProject;
+	private IPackageFragmentRoot packageRoot;
+	private IPackageFragment sourcePackage;
+	private IPackageFragment targetPackage;
+	private List<IType> typesToHide;
+
+	private IJavaSearchScope projectScope;
+
+	private final SearchEngine searchEngine = new SearchEngine();
+	private final Map<ICompilationUnit, Set<IMethod>> units = new LinkedHashMap<ICompilationUnit, Set<IMethod>>();
+	private final List<Change> changes = new LinkedList<Change>();
+	private final CodeFormatter codeFormatter = ToolFactory.createCodeFormatter(null);
+
+	public SplitRefactoringModel(String sourcePackageName, String targetPackageName, String targetSuffix,
+			List<String> typesToHideNames) {
+		this.sourcePackageName = sourcePackageName;
+		this.targetPackageName = targetPackageName;
+		this.targetSuffix = targetSuffix;
+		this.typesToHideNames = typesToHideNames;
+	}
+
+	public String getSourcePackageName() {
+		return sourcePackageName;
+	}
+
+	public void setSourcePackageName(String sourcePackageName) {
+		this.sourcePackageName = sourcePackageName;
+	}
+
+	public String getTargetPackageName() {
+		return targetPackageName;
+	}
+
+	public void setTargetPackageName(String targetPackageName) {
+		this.targetPackageName = targetPackageName;
+	}
+
+	public String getTargetSuffix() {
+		return targetSuffix;
+	}
+
+	public void setTargetSuffix(String targetSuffix) {
+		this.targetSuffix = targetSuffix;
+	}
+
+	public List<String> getTypesToHideNames() {
+		return typesToHideNames;
+	}
+
+	public void setTypesToHideNames(List<String> typesToHideNames) {
+		this.typesToHideNames = typesToHideNames;
+	}
+
+	public IStructuredSelection getSelection() {
+		return selection;
+	}
+
+	public void setSelection(IStructuredSelection selection) {
+		this.selection = selection;
+	}
+
+	public IProject getProject() {
+		return project;
+	}
+
+	public void setProject(IProject project) {
+		this.project = project;
+	}
+
+	public IJavaProject getJavaProject() {
+		return javaProject;
+	}
+
+	public void setJavaProject(IJavaProject javaProject) {
+		this.javaProject = javaProject;
+	}
+
+	public IPackageFragmentRoot getPackageRoot() {
+		return packageRoot;
+	}
+
+	public void setPackageRoot(IPackageFragmentRoot packageRoot) {
+		this.packageRoot = packageRoot;
+	}
+
+	public IPackageFragment getSourcePackage() {
+		return sourcePackage;
+	}
+
+	public void setSourcePackage(IPackageFragment sourcePackage) {
+		this.sourcePackage = sourcePackage;
+	}
+
+	public IPackageFragment getTargetPackage() {
+		return targetPackage;
+	}
+
+	public void setTargetPackage(IPackageFragment targetPackage) {
+		this.targetPackage = targetPackage;
+	}
+
+	public List<IType> getTypesToHide() {
+		return typesToHide;
+	}
+
+	public void setTypesToHide(List<IType> typesToHide) {
+		this.typesToHide = typesToHide;
+	}
+
+	public SearchEngine getSearchEngine() {
+		return searchEngine;
+	}
+
+	public IJavaSearchScope getProjectScope() {
+		return projectScope;
+	}
+
+	public void setProjectScope(IJavaSearchScope projectScope) {
+		this.projectScope = projectScope;
+	}
+
+	public Map<ICompilationUnit, Set<IMethod>> getUnits() {
+		return units;
+	}
+
+	public List<Change> getChanges() {
+		return changes;
+	}
+
+	public CodeFormatter getCodeFormatter() {
+		return codeFormatter;
+	}
+
+}
