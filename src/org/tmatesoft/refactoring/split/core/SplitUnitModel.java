3870b87878c60f73a1634aa1cc7fade21c937216
sergey.scherbina <sergey.scherbina@0a862816-5deb-0310-9199-c792c6ae6c6e>
2010-03-05 11:39:52 +0000

    Split script implementation in progress.

--- src/org/tmatesoft/refactoring/split/core/SplitUnitModel.java
+++ src/org/tmatesoft/refactoring/split/core/SplitUnitModel.java
@@ -1,5 +1,8 @@
 package org.tmatesoft.refactoring.split.core;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -219,136 +222,6 @@ public class SplitUnitModel {
 
 	}
 
-	public void applyUnitSplit(final RefactoringStatus status, final IProgressMonitor monitor) throws CoreException,
-			MalformedTreeException, BadLocationException {
-
-		final String typeName = model.addTargetSuffix(sourceTypeName);
-		final String unitName = typeName + ".java";
-
-		final ICompilationUnit unit = model.getTargetPackage().getCompilationUnit(unitName);
-		if (!unit.exists()) {
-
-			final AST ast = AST.newAST(AST.JLS3);
-			final CompilationUnit node = ast.newCompilationUnit();
-
-			final PackageDeclaration packageDeclaration = ast.newPackageDeclaration();
-			packageDeclaration.setName(ast.newName(model.getTargetPackage().getElementName()));
-			node.setPackage(packageDeclaration);
-
-			final TypeDeclaration type = ast.newTypeDeclaration();
-			node.types().add(type);
-
-			type.setInterface(isSourceInterface);
-			final List modifiers = type.modifiers();
-			modifiers.add(ast.newModifier(Modifier.ModifierKeyword.PUBLIC_KEYWORD));
-			if (isSourceAbstractClass) {
-				modifiers.add(ast.newModifier(Modifier.ModifierKeyword.ABSTRACT_KEYWORD));
-			}
-			type.setName(ast.newSimpleName(typeName));
-
-			if (sourceSuperClassMetadata != null) {
-				final IType sourceSuperClassType = sourceSuperClassMetadata.getType();
-				if (sourceSuperClassType != null) {
-					final Type sourceSuperClassNode = sourceSuperClassMetadata.getTypeNode();
-					final ICompilationUnit sourceSuperClassUnit = sourceSuperClassMetadata.getUnit();
-					if (model.getUnits().containsKey(sourceSuperClassUnit)) {
-						final String sourceSuperClassName = sourceSuperClassMetadata.getName();
-						final String targetSuperclassName = model.addTargetSuffix(sourceSuperClassName);
-						type.setSuperclassType(ast.newSimpleType(ast.newName(targetSuperclassName)));
-					} else {
-						getUsedTypes().add(sourceSuperClassType);
-						type.setSuperclassType((Type) ASTNode.copySubtree(ast, sourceSuperClassNode));
-					}
-				}
-			}
-
-			final List<Type> superInterfaceTypes = type.superInterfaceTypes();
-			if (!sourceSuperInterfacesMetadata.isEmpty()) {
-				for (final TypeMetadata sourceSuperInterface : sourceSuperInterfacesMetadata.values()) {
-					final IType sourceSuperInterfaceType = sourceSuperInterface.getType();
-					if (sourceSuperInterfaceType != null) {
-						final Type sourceSuperInterfaceNode = sourceSuperInterface.getTypeNode();
-						final ICompilationUnit sourceSuperInterfaceUnit = sourceSuperInterface.getUnit();
-						if (model.getUnits().containsKey(sourceSuperInterfaceUnit)) {
-							final String sourceSuperInterfaceName = sourceSuperInterface.getName();
-							final String targetSuperInterfaceName = model.addTargetSuffix(sourceSuperInterfaceName);
-							superInterfaceTypes.add(ast.newSimpleType(ast.newName(targetSuperInterfaceName)));
-						} else {
-							getUsedTypes().add(sourceSuperInterfaceType);
-							superInterfaceTypes.add((Type) ASTNode.copySubtree(ast, sourceSuperInterfaceNode));
-						}
-					}
-				}
-			}
-
-			final List imports = node.imports();
-			for (final IType usedType : getUsedTypes()) {
-				final IPackageFragment usedPackage = usedType.getPackageFragment();
-				if (!"java.lang".equals(usedPackage.getElementName())) {
-					final ImportDeclaration importDeclaration = ast.newImportDeclaration();
-					importDeclaration.setOnDemand(false);
-					importDeclaration.setName(ast.newQualifiedName(ast.newName(usedPackage.getElementName()), ast
-							.newSimpleName(usedType.getElementName())));
-					imports.add(importDeclaration);
-				}
-			}
-
-			final List bodyDeclarations = type.bodyDeclarations();
-
-			for (final IField sourceField : getUsedFields()) {
-				final FieldDeclaration sourceFieldNode = (FieldDeclaration) NodeFinder.perform(sourceAst, sourceField
-						.getSourceRange());
-				final FieldDeclaration fieldDeclarationCopy = (FieldDeclaration) ASTNode.copySubtree(ast,
-						sourceFieldNode);
-				bodyDeclarations.add(fieldDeclarationCopy);
-			}
-
-			for (final MethodDeclaration sourceMethodDeclaration : getAddMethods().values()) {
-
-				final MethodDeclaration methodCopy = (MethodDeclaration) ASTNode.copySubtree(ast,
-						sourceMethodDeclaration);
-
-				if (sourceMethodDeclaration.isConstructor()) {
-					methodCopy.setName(ast.newSimpleName(model.addTargetSuffix(sourceTypeName)));
-				}
-
-				final IMethodBinding sourceMethodBinding = sourceMethodDeclaration.resolveBinding();
-				final String from = sourceMethodBinding.getDeclaringClass().getQualifiedName();
-
-				Javadoc javadoc = methodCopy.getJavadoc();
-				if (javadoc == null) {
-					javadoc = ast.newJavadoc();
-					methodCopy.setJavadoc(javadoc);
-				}
-				final TagElement tag = ast.newTagElement();
-				tag.setTagName("@from ");
-				final TextElement text = ast.newTextElement();
-				text.setText(from);
-				tag.fragments().add(text);
-				javadoc.tags().add(tag);
-
-				bodyDeclarations.add(methodCopy);
-			}
-
-			for (final IType sourceNestedType : getNestedTypes()) {
-				final TypeDeclaration sourceNestedTypeNode = (TypeDeclaration) NodeFinder.perform(sourceAst,
-						sourceNestedType.getSourceRange());
-				final TypeDeclaration sourceNestedTypeCopy = (TypeDeclaration) ASTNode.copySubtree(ast,
-						sourceNestedTypeNode);
-				bodyDeclarations.add(sourceNestedTypeCopy);
-			}
-
-			final String source = node.toString();
-			final Document document = new Document(source);
-			final TextEdit formatEdit = model.getCodeFormatter().format(CodeFormatter.K_COMPILATION_UNIT, source, 0,
-					source.length(), 0, model.getJavaProject().findRecommendedLineSeparator());
-			formatEdit.apply(document);
-
-			model.getChanges().add(new CreateCompilationUnitChange(unit, document.get(), null));
-
-		}
-	}
-
 	public void buildModel(final SplitRefactoringModel model) throws JavaModelException {
 		addConstructors();
 		addSuperMethods();
@@ -417,6 +290,149 @@ public class SplitUnitModel {
 	public void addMethodToUnitModel(final IMethod sourceMethod) throws JavaModelException {
 		final SplitUnitAddMethodBuilder builder = new SplitUnitAddMethodBuilder(this, sourceMethod);
 		builder.addMethodToUnit();
+	}
+
+	public void applyUnitSplit(final RefactoringStatus status, final IProgressMonitor monitor) throws CoreException,
+			MalformedTreeException, BadLocationException {
+		final String typeName = model.addTargetSuffix(sourceTypeName);
+		final String unitName = typeName + ".java";
+		final ICompilationUnit targetUnit = model.getTargetPackage().getCompilationUnit(unitName);
+		if (!targetUnit.exists()) {
+			buildTargetUnit(typeName, targetUnit);
+		}
+	}
+
+	private void buildTargetUnit(final String typeName, final ICompilationUnit unit) throws JavaModelException,
+			BadLocationException {
+
+		final AST ast = AST.newAST(AST.JLS3);
+		final CompilationUnit node = ast.newCompilationUnit();
+
+		final PackageDeclaration packageDeclaration = ast.newPackageDeclaration();
+		packageDeclaration.setName(ast.newName(model.getTargetPackage().getElementName()));
+		node.setPackage(packageDeclaration);
+
+		final TypeDeclaration type = ast.newTypeDeclaration();
+		node.types().add(type);
+
+		type.setInterface(isSourceInterface);
+		final List modifiers = type.modifiers();
+		modifiers.add(ast.newModifier(Modifier.ModifierKeyword.PUBLIC_KEYWORD));
+		if (isSourceAbstractClass) {
+			modifiers.add(ast.newModifier(Modifier.ModifierKeyword.ABSTRACT_KEYWORD));
+		}
+		type.setName(ast.newSimpleName(typeName));
+
+		if (sourceSuperClassMetadata != null) {
+			final IType sourceSuperClassType = sourceSuperClassMetadata.getType();
+			if (sourceSuperClassType != null) {
+				final Type sourceSuperClassNode = sourceSuperClassMetadata.getTypeNode();
+				final ICompilationUnit sourceSuperClassUnit = sourceSuperClassMetadata.getUnit();
+				if (model.getUnits().containsKey(sourceSuperClassUnit)) {
+					final String sourceSuperClassName = sourceSuperClassMetadata.getName();
+					final String targetSuperclassName = model.addTargetSuffix(sourceSuperClassName);
+					type.setSuperclassType(ast.newSimpleType(ast.newName(targetSuperclassName)));
+				} else {
+					getUsedTypes().add(sourceSuperClassType);
+					type.setSuperclassType((Type) ASTNode.copySubtree(ast, sourceSuperClassNode));
+				}
+			}
+		}
+
+		final List<Type> superInterfaceTypes = type.superInterfaceTypes();
+		if (!sourceSuperInterfacesMetadata.isEmpty()) {
+			for (final TypeMetadata sourceSuperInterface : sourceSuperInterfacesMetadata.values()) {
+				final IType sourceSuperInterfaceType = sourceSuperInterface.getType();
+				if (sourceSuperInterfaceType != null) {
+					final Type sourceSuperInterfaceNode = sourceSuperInterface.getTypeNode();
+					final ICompilationUnit sourceSuperInterfaceUnit = sourceSuperInterface.getUnit();
+					if (model.getUnits().containsKey(sourceSuperInterfaceUnit)) {
+						final String sourceSuperInterfaceName = sourceSuperInterface.getName();
+						final String targetSuperInterfaceName = model.addTargetSuffix(sourceSuperInterfaceName);
+						superInterfaceTypes.add(ast.newSimpleType(ast.newName(targetSuperInterfaceName)));
+					} else {
+						getUsedTypes().add(sourceSuperInterfaceType);
+						superInterfaceTypes.add((Type) ASTNode.copySubtree(ast, sourceSuperInterfaceNode));
+					}
+				}
+			}
+		}
+
+		final List imports = node.imports();
+		final List<IType> usedTypesList = new ArrayList<IType>(getUsedTypes());
+		Collections.sort(usedTypesList, new Comparator<IType>() {
+			@Override
+			public int compare(IType t1, IType t2) {
+				final IPackageFragment p1 = t1.getPackageFragment();
+				final IPackageFragment p2 = t2.getPackageFragment();
+				final int p = p1.getElementName().compareTo(p2.getElementName());
+				if (p != 0) {
+					return p;
+				}
+				return t1.getElementName().compareTo(t2.getElementName());
+			}
+		});
+		for (final IType usedType : usedTypesList) {
+			final IPackageFragment usedPackage = usedType.getPackageFragment();
+			if (!"java.lang".equals(usedPackage.getElementName())) {
+				final ImportDeclaration importDeclaration = ast.newImportDeclaration();
+				importDeclaration.setOnDemand(false);
+				importDeclaration.setName(ast.newQualifiedName(ast.newName(usedPackage.getElementName()), ast
+						.newSimpleName(usedType.getElementName())));
+				imports.add(importDeclaration);
+			}
+		}
+
+		final List bodyDeclarations = type.bodyDeclarations();
+
+		for (final IField sourceField : getUsedFields()) {
+			final FieldDeclaration sourceFieldNode = (FieldDeclaration) NodeFinder.perform(sourceAst, sourceField
+					.getSourceRange());
+			final FieldDeclaration fieldDeclarationCopy = (FieldDeclaration) ASTNode.copySubtree(ast, sourceFieldNode);
+			bodyDeclarations.add(fieldDeclarationCopy);
+		}
+
+		for (final MethodDeclaration sourceMethodDeclaration : getAddMethods().values()) {
+
+			final MethodDeclaration methodCopy = (MethodDeclaration) ASTNode.copySubtree(ast, sourceMethodDeclaration);
+
+			if (sourceMethodDeclaration.isConstructor()) {
+				methodCopy.setName(ast.newSimpleName(model.addTargetSuffix(sourceTypeName)));
+			}
+
+			final IMethodBinding sourceMethodBinding = sourceMethodDeclaration.resolveBinding();
+			final String from = sourceMethodBinding.getDeclaringClass().getQualifiedName();
+
+			Javadoc javadoc = methodCopy.getJavadoc();
+			if (javadoc == null) {
+				javadoc = ast.newJavadoc();
+				methodCopy.setJavadoc(javadoc);
+			}
+			final TagElement tag = ast.newTagElement();
+			tag.setTagName("@from ");
+			final TextElement text = ast.newTextElement();
+			text.setText(from);
+			tag.fragments().add(text);
+			javadoc.tags().add(tag);
+
+			bodyDeclarations.add(methodCopy);
+		}
+
+		for (final IType sourceNestedType : getNestedTypes()) {
+			final TypeDeclaration sourceNestedTypeNode = (TypeDeclaration) NodeFinder.perform(sourceAst,
+					sourceNestedType.getSourceRange());
+			final TypeDeclaration sourceNestedTypeCopy = (TypeDeclaration) ASTNode.copySubtree(ast,
+					sourceNestedTypeNode);
+			bodyDeclarations.add(sourceNestedTypeCopy);
+		}
+
+		final String source = node.toString();
+		final Document document = new Document(source);
+		final TextEdit formatEdit = model.getCodeFormatter().format(CodeFormatter.K_COMPILATION_UNIT, source, 0,
+				source.length(), 0, model.getJavaProject().findRecommendedLineSeparator());
+		formatEdit.apply(document);
+
+		model.getChanges().add(new CreateCompilationUnitChange(unit, document.get(), null));
 	}
 
 }
