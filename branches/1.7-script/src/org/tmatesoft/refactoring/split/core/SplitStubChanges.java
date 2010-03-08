42c70eb251a644556d74bc63d1519f80dbb8cf5c
sergey.scherbina <sergey.scherbina@0a862816-5deb-0310-9199-c792c6ae6c6e>
2010-03-08 10:48:02 +0000

    Split script implementation in progress.

--- branches/1.7-script/src/org/tmatesoft/refactoring/split/core/SplitStubChanges.java
+++ branches/1.7-script/src/org/tmatesoft/refactoring/split/core/SplitStubChanges.java
@@ -3,12 +3,18 @@ package org.tmatesoft.refactoring.split.core;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.jdt.core.dom.PrimitiveType;
+
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.Block;
+import org.eclipse.jdt.core.dom.Expression;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.ReturnStatement;
 import org.eclipse.jdt.core.dom.Statement;
 import org.eclipse.jdt.core.dom.SuperConstructorInvocation;
+import org.eclipse.jdt.core.dom.Type;
+import org.eclipse.jdt.core.dom.TypeLiteral;
+import org.eclipse.jdt.core.dom.PrimitiveType.Code;
 
 public class SplitStubChanges extends SplitMoveChanges {
 
@@ -17,22 +23,49 @@ public class SplitStubChanges extends SplitMoveChanges {
 	}
 
 	@Override
-	protected MethodDeclaration getMethodCopy(AST ast, MethodDeclaration sourceMethodDeclaration) {
+	protected MethodDeclaration getMethodCopy(final AST ast, final MethodDeclaration sourceMethodDeclaration) {
 		final MethodDeclaration methodCopy = super.getMethodCopy(ast, sourceMethodDeclaration);
 		final Block methodBody = methodCopy.getBody();
 		if (methodBody != null) {
 			final List<Statement> statements = methodBody.statements();
 			if (statements != null) {
+
 				final List<Statement> emptyBody = new ArrayList<Statement>();
-				for (final Statement statement : statements) {
-					if (statement instanceof SuperConstructorInvocation) {
-						emptyBody.add(statement);
+
+				if (methodCopy.isConstructor()) {
+					for (final Statement statement : statements) {
+						if (statement instanceof SuperConstructorInvocation) {
+							emptyBody.add(statement);
+						}
+					}
+				} else {
+					final Type returnType = methodCopy.getReturnType2();
+					final ReturnStatement newReturnStatement = ast.newReturnStatement();
+					if (returnType instanceof PrimitiveType) {
+						final PrimitiveType returnPrimitive = (PrimitiveType) returnType;
+						final Code code = returnPrimitive.getPrimitiveTypeCode();
+						if (code != PrimitiveType.VOID) {
+							if (code == PrimitiveType.BYTE || code == PrimitiveType.CHAR || code == PrimitiveType.SHORT
+									|| code == PrimitiveType.INT || code == PrimitiveType.LONG) {
+								newReturnStatement.setExpression(ast.newNumberLiteral("0"));
+							} else if (code == PrimitiveType.FLOAT || code == PrimitiveType.DOUBLE) {
+								newReturnStatement.setExpression(ast.newNumberLiteral("0.0"));
+							} else if (code == PrimitiveType.BOOLEAN) {
+								newReturnStatement.setExpression(ast.newBooleanLiteral(false));
+							}
+							emptyBody.add(newReturnStatement);
+						}
+					} else {
+						newReturnStatement.setExpression(ast.newNullLiteral());
+						emptyBody.add(newReturnStatement);
 					}
 				}
+
 				statements.clear();
 				if (!emptyBody.isEmpty()) {
 					statements.addAll(emptyBody);
 				}
+
 			}
 		}
 		return methodCopy;
