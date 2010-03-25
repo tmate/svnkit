6f9550e5a892d5d77f44eb61093351f114f448e4
sergey.scherbina <sergey.scherbina@0a862816-5deb-0310-9199-c792c6ae6c6e>
2010-03-25 23:31:41 +0000

    Create project for Split2 refactoring script.

---
+++ src/org/tmatesoft/refactoring/split2/actions/Split2RefactoringAction.java
@@ -0,0 +1,44 @@
+package org.tmatesoft.refactoring.split2.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+
+public class Split2RefactoringAction implements IObjectActionDelegate {
+
+	private Shell shell;
+	
+	/**
+	 * Constructor for Action1.
+	 */
+	public Split2RefactoringAction() {
+		super();
+	}
+
+	/**
+	 * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
+	 */
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		shell = targetPart.getSite().getShell();
+	}
+
+	/**
+	 * @see IActionDelegate#run(IAction)
+	 */
+	public void run(IAction action) {
+		MessageDialog.openInformation(
+			shell,
+			"Split2",
+			"Split2 refactoring was executed.");
+	}
+
+	/**
+	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+	}
+
+}
