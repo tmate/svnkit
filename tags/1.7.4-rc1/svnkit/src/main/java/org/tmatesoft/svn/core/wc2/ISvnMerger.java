fb8df7910711f5ce21c677fad16cbe026942bc9f
alex <alex@0a862816-5deb-0310-9199-c792c6ae6c6e>
2012-03-20 23:29:08 +0000

    SVNKit version 1.7.4-rc1

---
+++ tags/1.7.4-rc1/svnkit/src/main/java/org/tmatesoft/svn/core/wc2/ISvnMerger.java
@@ -0,0 +1,45 @@
+package org.tmatesoft.svn.core.wc2;
+
+import java.io.File;
+
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.SVNNodeKind;
+import org.tmatesoft.svn.core.SVNProperties;
+import org.tmatesoft.svn.core.internal.wc.SVNConflictVersion;
+import org.tmatesoft.svn.core.wc.ISVNMerger;
+import org.tmatesoft.svn.core.wc.SVNDiffOptions;
+
+/**
+ * Implement this interface additionally to ISVNMerger, to provide custom text and properties merge code
+ * for the 1.7 working copy format.
+ * 
+ * @author TMate Software Ltd.
+ * @version 1.7
+ */
+public interface ISvnMerger extends ISVNMerger {
+    
+    public SvnMergeResult mergeText(
+            ISvnMerger baseMerger,
+            File resultFile, 
+            File targetAbspath, 
+            File detranslatedTargetAbspath, 
+            File leftAbspath, 
+            File rightAbspath, 
+            String targetLabel, 
+            String leftLabel, 
+            String rightLabel, 
+            SVNDiffOptions options) throws SVNException;
+    
+    public SvnMergeResult mergeProperties(
+            ISvnMerger baseMerger,
+            File localAbsPath, 
+            SVNNodeKind kind, 
+            SVNConflictVersion leftVersion, 
+            SVNConflictVersion rightVersion,
+            SVNProperties serverBaseProperties, 
+            SVNProperties pristineProperties, 
+            SVNProperties actualProperties, 
+            SVNProperties propChanges,
+            boolean baseMerge, 
+            boolean dryRun) throws SVNException;
+}
