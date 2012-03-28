7b6a6b48a6b6846bfff2350d604c049127da950a
alex <alex@0a862816-5deb-0310-9199-c792c6ae6c6e>
2012-03-28 17:59:19 +0000

    SVNKit version 1.7.4-rc3

---
+++ tags/1.7.4-rc3/svnkit/src/main/java/org/tmatesoft/svn/core/wc2/SvnSwitch.java
@@ -0,0 +1,154 @@
+package org.tmatesoft.svn.core.wc2;
+
+import org.tmatesoft.svn.core.SVNDepth;
+import org.tmatesoft.svn.core.SVNErrorCode;
+import org.tmatesoft.svn.core.SVNErrorMessage;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.internal.wc.SVNErrorManager;
+import org.tmatesoft.svn.core.wc.ISVNEventHandler;
+import org.tmatesoft.svn.core.wc.SVNRevision;
+import org.tmatesoft.svn.util.SVNLogType;
+
+/**
+ * Represents switch operation.
+ * Switches working tree of <code>target</code> to <code>switchTarget</code>\
+ * <code>switchTarget</code>'s <code>pegRevision</code> at <code>revision</code>.
+ * 
+ * <p/>
+ * Summary of purpose: this is normally used to switch a working directory
+ * over to another line of development, such as a branch or a tag. Switching
+ * an existing working directory is more efficient than checking out
+ * <code>switchTarget</code> from scratch.
+ * 
+ * <p/>
+ * <code>revision</code> must represent a valid revision number (
+ * {@link SVNRevision#getNumber()} >= 0), or date (
+ * {@link SVNRevision#getDate()} != <span class="javakeyword">true</span>),
+ * or be equal to {@link SVNRevision#HEAD}. If <code>revision</code> does
+ * not meet these requirements, an exception with the error code
+ * {@link SVNErrorCode#CLIENT_BAD_REVISION} is thrown.
+ * 
+ * <p/>
+ * If <code>depth</code> is {@link SVNDepth#INFINITY}, switches fully
+ * recursively. Else if it is {@link SVNDepth#IMMEDIATES}, switches
+ * <code>target</code> and its file children (if any), and switches
+ * subdirectories but does not update them. Else if {@link SVNDepth#FILES},
+ * switches just file children, ignoring subdirectories completely. Else if
+ * {@link SVNDepth#EMPTY}, switches just <code>target</code> and touches
+ * nothing underneath it.
+ * 
+ * <p/>
+ * If externals are {@link #isIgnoreExternals() ignored}, doesn't process
+ * externals definitions as part of this operation.
+ * 
+ * <p/>
+ * If <code>allowUnversionedObstructions</code> is <span
+ * class="javakeyword">true</span> then the switch tolerates existing
+ * unversioned items that obstruct added paths. Only obstructions of the
+ * same type (file or directory) as the added item are tolerated. The text of
+ * obstructing files is left as-is, effectively treating it as a user
+ * modification after the switch. Working properties of obstructing items
+ * are set equal to the base properties. If
+ * <code>allowUnversionedObstructions</code> is <span
+ * class="javakeyword">false</span> then the switch will abort if there are
+ * any unversioned obstructing items.
+ * 
+ * <p/>
+ * If the caller's {@link ISVNEventHandler} is non-<span
+ * class="javakeyword">null</span>, it is invoked for paths affected by the
+ * switch, and also for files restored from text-base. Also
+ * {@link ISVNEventHandler#checkCancelled()} will be used at various places
+ * during the switch to check whether the caller wants to stop the switch.
+ * 
+ * <p/>
+ * This operation requires repository access (in case the repository is not
+ * on the same machine, network connection is established).
+ * 
+ * <p/>
+ * {@link #run()} method returns value of the revision to which the working copy was actually switched.
+ * 
+ * @author TMate Software Ltd.
+ * @version 1.7
+ */
+public class SvnSwitch extends AbstractSvnUpdate<Long> {
+
+    private boolean depthIsSticky;
+    private boolean ignoreAncestry;
+    
+    private SvnTarget switchTarget;
+
+    protected SvnSwitch(SvnOperationFactory factory) {
+        super(factory);
+    }
+
+    /**
+     * Returns whether depth is sticky.
+     * If <code>depthIsSticky</code> is set the operation will use <code>depth</code> as status scope, otherwise 
+     * {@link SVNDepth#UNKNOWN} will be used.
+	 * 
+     * @return <code>true</code> if the depth is sticky, otherwise <code>false</code>
+     */
+    public boolean isDepthIsSticky() {
+        return depthIsSticky;
+    }
+
+    /**
+     * Returns whether to ignore ancestry when calculating merges.
+     * 
+     * @return <code>true</code> if ancestry should be ignored, otherwise <code>false</code>
+     * @since 1.7, Subversion 1.7
+     */
+    public boolean isIgnoreAncestry() {
+        return ignoreAncestry;
+    }
+
+    /**
+     * Returns the repository location as a target against which the item will be switched.
+     * 
+     * @return switch target
+     */
+    public SvnTarget getSwitchTarget() {
+        return switchTarget;
+    }
+
+    /**
+     * Sets whether depth is sticky.
+     * If <code>depthIsSticky</code> is set the operation will use <code>depth</code> as status scope, otherwise 
+     * {@link SVNDepth#UNKNOWN} will be used.
+	 * 
+     * @param depthIsSticky <code>true</code> if the depth is sticky, otherwise <code>false</code>
+     */
+    public void setDepthIsSticky(boolean depthIsSticky) {
+        this.depthIsSticky = depthIsSticky;
+    }
+
+    /**
+     * Sets whether to ignore ancestry when calculating merges.
+     * 
+     * @param ignoreAncestry <code>true</code> if ancestry should be ignored, otherwise <code>false</code>
+     * @since 1.7, Subversion 1.7
+     */
+    public void setIgnoreAncestry(boolean ignoreAncestry) {
+        this.ignoreAncestry = ignoreAncestry;
+    }
+
+    /**
+     * Sets the repository location as a target against which the item will be switched.
+     * 
+     * @param switchTarget switch target
+     */
+    public void setSwitchTarget(SvnTarget switchTarget) {
+        this.switchTarget = switchTarget;
+    }
+
+    @Override
+    protected void ensureArgumentsAreValid() throws SVNException {
+        super.ensureArgumentsAreValid();
+        if (getDepth() == SVNDepth.EXCLUDE) {
+            SVNErrorMessage err = SVNErrorMessage.create(SVNErrorCode.UNSUPPORTED_FEATURE, "Cannot both exclude and switch a path");
+            SVNErrorManager.error(err, SVNLogType.WC);
+        }
+    }
+    
+    
+}
