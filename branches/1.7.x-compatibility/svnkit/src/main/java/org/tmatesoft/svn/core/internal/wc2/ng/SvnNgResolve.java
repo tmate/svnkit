c3658dd665befe097cae051f130886440d5d4dbb
Dmitry Pavlenko <Dmitry Pavlenko@0a862816-5deb-0310-9199-c792c6ae6c6e>
2014-01-21 14:29:32 +0000

    SVNKIT-430: Inherited properties shouldn't be read for working copies of format <1.8.

---
+++ branches/1.7.x-compatibility/svnkit/src/main/java/org/tmatesoft/svn/core/internal/wc2/ng/SvnNgResolve.java
@@ -0,0 +1,33 @@
+package org.tmatesoft.svn.core.internal.wc2.ng;
+
+import org.tmatesoft.svn.core.SVNErrorCode;
+import org.tmatesoft.svn.core.SVNErrorMessage;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.internal.wc.SVNErrorManager;
+import org.tmatesoft.svn.core.internal.wc17.SVNWCContext;
+import org.tmatesoft.svn.core.wc2.SvnResolve;
+import org.tmatesoft.svn.util.SVNLogType;
+
+import java.io.File;
+
+public class SvnNgResolve extends SvnNgOperationRunner<Void, SvnResolve>  {
+
+    @Override
+    protected Void run(SVNWCContext context) throws SVNException {
+    	if (getOperation().getFirstTarget().isURL()) {
+        	SVNErrorMessage err = SVNErrorMessage.create(SVNErrorCode.ILLEGAL_TARGET,
+        			"'{0}' is not a local path", getOperation().getFirstTarget().getURL());
+            SVNErrorManager.error(err, SVNLogType.WC);
+        }
+        File localAbsPath = getFirstTarget();
+        File lockAbsPath = context.acquireWriteLockForResolve(localAbsPath);
+        context.resolvedConflict(getFirstTarget(), getOperation().getDepth(), true, "", true, getOperation().getConflictChoice());
+        try {
+        context.releaseWriteLock(lockAbsPath);
+        } finally {
+            sleepForTimestamp();
+        }
+    	return null;
+    }
+
+}
