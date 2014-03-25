2587e84ba346748d8ad163d8a24bfe1bbf36990e
Dmitry Pavlenko <Dmitry Pavlenko@0a862816-5deb-0310-9199-c792c6ae6c6e>
2014-03-25 15:20:37 +0000

    Now SvnCheckout#targetWorkingCopyFormat is taken into account for format < format(1.7).

--- trunk/svnkit/src/main/java/org/tmatesoft/svn/core/internal/wc2/ng/SvnNgCheckout.java
+++ trunk/svnkit/src/main/java/org/tmatesoft/svn/core/internal/wc2/ng/SvnNgCheckout.java
@@ -4,10 +4,20 @@ package org.tmatesoft.svn.core.internal.wc2.ng;
 import org.tmatesoft.svn.core.SVNException;
 import org.tmatesoft.svn.core.internal.wc17.SVNWCContext;
 import org.tmatesoft.svn.core.internal.wc17.db.ISVNWCDb;
+import org.tmatesoft.svn.core.internal.wc2.SvnWcGeneration;
 import org.tmatesoft.svn.core.wc2.SvnCheckout;
 import org.tmatesoft.svn.core.wc2.SvnTarget;
 
-public class SvnNgCheckout extends SvnNgAbstractUpdate<Long, SvnCheckout>{
+public class SvnNgCheckout extends SvnNgAbstractUpdate<Long, SvnCheckout> {
+
+    @Override
+    public boolean isApplicable(SvnCheckout operation, SvnWcGeneration wcGeneration) throws SVNException {
+        final int targetWorkingCopyFormat = operation.getTargetWorkingCopyFormat();
+        if (targetWorkingCopyFormat > 0) {
+            return targetWorkingCopyFormat >= SVNWCContext.WC_NG_VERSION;
+        }
+        return super.isApplicable(operation, wcGeneration);
+    }
 
     @Override
     protected Long run(SVNWCContext context) throws SVNException {
