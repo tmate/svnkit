8c98e57461a67e7dc0e31805ddfdb078bb542e13
Dmitry Pavlenko <Dmitry Pavlenko@0a862816-5deb-0310-9199-c792c6ae6c6e>
2014-07-17 17:26:46 +0000

    SVNKIT-529: Added reproducing test, currently fails.

--- svnkit/src/test/java/org/tmatesoft/svn/test/ListTest.java
+++ svnkit/src/test/java/org/tmatesoft/svn/test/ListTest.java
@@ -2,12 +2,18 @@ package org.tmatesoft.svn.test;
 
 import org.junit.Assert;
 import org.junit.Assume;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.tmatesoft.svn.core.SVNDirEntry;
 import org.tmatesoft.svn.core.SVNException;
 import org.tmatesoft.svn.core.SVNURL;
+import org.tmatesoft.svn.core.internal.io.fs.FSFS;
 import org.tmatesoft.svn.core.internal.wc2.SvnWcGeneration;
+import org.tmatesoft.svn.core.io.SVNRepository;
+import org.tmatesoft.svn.core.io.SVNRepositoryFactory;
+import org.tmatesoft.svn.core.wc.SVNClientManager;
 import org.tmatesoft.svn.core.wc.SVNRevision;
+import org.tmatesoft.svn.core.wc.admin.SVNAdminClient;
 import org.tmatesoft.svn.core.wc2.ISvnObjectReceiver;
 import org.tmatesoft.svn.core.wc2.SvnList;
 import org.tmatesoft.svn.core.wc2.SvnOperationFactory;
@@ -15,6 +21,7 @@ import org.tmatesoft.svn.core.wc2.SvnTarget;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -205,11 +212,83 @@ public class ListTest {
             final SVNDirEntry dirEntry = list.run();
 
             Assert.assertEquals(url, dirEntry.getURL());
-
         } finally {
             svnOperationFactory.dispose();
             sandbox.dispose();
         }
+    }
+
+    @Ignore("SVNKIT-529")
+    @Test
+    public void testGetDirHangsLongCommitMessage() throws Exception {
+        final TestOptions options = TestOptions.getInstance();
+
+        final long oldMaxFilesPerDirectory = FSFS.getDefaultMaxFilesPerDirectory();
+
+        final SvnOperationFactory svnOperationFactory = new SvnOperationFactory();
+        final Sandbox sandbox = Sandbox.createWithCleanup(getTestName() + ".testGetDirHangsLongCommitMessage", options);
+        try {
+            FSFS.setDefaultMaxFilesPerDirectory(3); //3 < 5(number of revisions), so the repository will be packed
+            final File repositoryDirectory = sandbox.createDirectory("svn.repo");
+
+            final SVNURL url;
+            final SVNClientManager clientManager = SVNClientManager.newInstance();
+            try {
+                SVNAdminClient adminClient = clientManager.getAdminClient();
+                adminClient.doCreateRepository(repositoryDirectory, null, true, false, false, false, false, false, true);
+
+                url = SVNURL.fromFile(repositoryDirectory);
+
+                final CommitBuilder commitBuilder1 = new CommitBuilder(url);
+                commitBuilder1.addDirectory("directory1");
+                commitBuilder1.commit();
+
+                final CommitBuilder commitBuilder2 = new CommitBuilder(url);
+                commitBuilder2.addDirectory("directory2");
+                commitBuilder2.commit();
+
+                final CommitBuilder commitBuilder3 = new CommitBuilder(url);
+                commitBuilder3.addDirectory("directory3");
+                commitBuilder3.commit();
+
+                final CommitBuilder commitBuilder4 = new CommitBuilder(url);
+                commitBuilder4.addDirectory("directory4");
+                commitBuilder4.commit();
+
+                final CommitBuilder commitBuilder5 = new CommitBuilder(url);
+                commitBuilder5.addDirectory("directory5/file");
+                commitBuilder5.setCommitMessage(createStringForLength(1936)); //large number >> 1024
+                commitBuilder5.commit();
+
+                adminClient.doPack(new File(url.getPath()));
+            } finally {
+                clientManager.dispose();
+            }
+
+            final SVNRepository svnRepository = SVNRepositoryFactory.create(url);
+            try {
+                final List<SVNDirEntry> dirEntries = new ArrayList<SVNDirEntry>();
+                svnRepository.getDir("directory5", 5, null, (Collection) dirEntries);
+
+                Assert.assertEquals(1, dirEntries.size());
+                Assert.assertEquals("file", dirEntries.get(0).getName());
+            } finally {
+                svnRepository.closeSession();
+            }
+        } finally {
+            FSFS.setDefaultMaxFilesPerDirectory(oldMaxFilesPerDirectory);
+
+            svnOperationFactory.dispose();
+            sandbox.dispose();
+        }
+    }
+
+    private String createStringForLength(int length) {
+        final StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            stringBuilder.append('a');
+        }
+        return stringBuilder.toString();
     }
 
     private String getTestName() {
