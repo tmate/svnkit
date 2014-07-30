076a04942cba5b83b8b244730fe1115f7ba2ca97
alex <alex@0a862816-5deb-0310-9199-c792c6ae6c6e>
2014-07-30 19:45:01 +0000

    SVNKit 1.8.6

---
+++ 1.8.6/svnkit/src/main/java/org/tmatesoft/svn/core/internal/io/dav/http/NTLMEngineException.java
@@ -0,0 +1,16 @@
+package org.tmatesoft.svn.core.internal.io.dav.http;
+
+import java.io.IOException;
+
+/**
+* Created by alex on 7/3/14.
+*/
+public class NTLMEngineException extends IOException {
+    public NTLMEngineException(String message) {
+        super(message);
+    }
+
+    public NTLMEngineException(String message, Exception cause) {
+        super(message, cause);
+    }
+}
