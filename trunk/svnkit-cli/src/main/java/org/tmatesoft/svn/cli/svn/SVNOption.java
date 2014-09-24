2d9eca332836eb1a951502c15981884e5804aede
Dmitry Pavlenko <Dmitry Pavlenko@0a862816-5deb-0310-9199-c792c6ae6c6e>
2014-09-24 15:52:14 +0000

    SVNKIT-544: diff --ignore-properties option was not enabled in SVNKit, now fixed.

--- trunk/svnkit-cli/src/main/java/org/tmatesoft/svn/cli/svn/SVNOption.java
+++ trunk/svnkit-cli/src/main/java/org/tmatesoft/svn/cli/svn/SVNOption.java
@@ -104,6 +104,7 @@ public class SVNOption extends AbstractSVNOption {
     public static final SVNOption ALLOW_MIXED_REVISIONS = new SVNOption("allow-mixed-revisions");
     public static final SVNOption INCLUDE_EXTERNALS = new SVNOption("include-externals");
     public static final SVNOption PROPERTIES_ONLY = new SVNOption("properties-only");
+    public static final SVNOption IGNORE_PROPERTIES = new SVNOption("ignore-properties");
     public static final SVNOption REVERSE_DIFF = new SVNOption("reverse-diff");
     public static final SVNOption IGNORE_WHITESPACE = new SVNOption("ignore-whitespace");
 
